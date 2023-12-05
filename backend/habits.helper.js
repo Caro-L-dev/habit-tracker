853faeab4b62360d82c312bef38ef0fd75d62368
Caro-L-dev <liardcar@gmail.com>
2023-12-05 17:23:13 +0100

    feat: setup backend server

--- backend/habits.helper.js
+++ backend/habits.helper.js
@@ -9,7 +9,66 @@ const readDatabase = async () => {
   return json;
 };
 
+const writeDatabase = async (newDatabase) => {
+  const database = await readDatabase();
+
+  await fs.writeFile(
+    databaseFile,
+    JSON.stringify(
+      {
+        ...database,
+        ...newDatabase,
+      },
+      null,
+      2
+    )
+  );
+};
+
 export const getHabits = async () => {
   const database = await readDatabase();
   return database.habits;
+};
+
+export const getTodayHabits = async () => {
+  const today = new Date().toISOString().slice(0, 10);
+  const habits = await getHabits();
+
+  return habits.map((habit) => {
+    return {
+      id: habit.id,
+      title: habit.title,
+      done: habit.daysDone[today] || false,
+    };
+  });
+};
+
+export const addHabit = async (title) => {
+  const habits = await getHabits();
+
+  const newHabit = {
+    id: (habits[habits.length - 1].id || 0) + 1,
+    title,
+    daysDone: {},
+  };
+
+  habits.push(newHabit);
+
+  await writeDatabase({ habits });
+  return newHabit;
+};
+
+export const updateHabit = async (habitId, done) => {
+  const habits = await getHabits();
+  const toEditHabit = habits.find((a) => a.id === habitId);
+
+  if (!toEditHabit) {
+    throw new Error("habitId is invalid");
+  }
+
+  const today = new Date().toISOString().slice(0, 10);
+  toEditHabit.daysDone[today] = done;
+
+  await writeDatabase({ habits });
+  return toEditHabit;
 };
--- backend/routes/habits.js
+++ backend/routes/habits.js
@@ -1,8 +1,75 @@
-import { getHabits } from "../habits.helper.js";
+import {
+  getHabits,
+  getTodayHabits,
+  addHabit,
+  updateHabit,
+} from "../habits.helper.js";
 
 export async function habitsRoute(fastify) {
-  fastify.get("/", async () => {
-    const habits = await getHabits();
-    return habits;
+  fastify.get("/", async (request, reply) => {
+    try {
+      const habits = await getHabits();
+      return habits;
+    } catch (error) {
+      reply.code(400).send({
+        error: error.message,
+      });
+    }
+  });
+
+  fastify.get("/today", async (request, reply) => {
+    try {
+      const todayHabits = await getTodayHabits();
+      return todayHabits;
+    } catch (error) {
+      reply.code(400).send({
+        error: error.message,
+      });
+    }
+  });
+
+  fastify.post("/:habitId", async (request, reply) => {
+    const body = request.body;
+
+    if (body.title === undefined) {
+      reply.code(400).send({
+        error: "Title is required in the body",
+      });
+    }
+
+    const newHabit = await addHabit(body.title);
+    return newHabit;
+  });
+
+  fastify.patch("/:habitId", async (request, reply) => {
+    const body = request.body;
+
+    if (body.done === undefined) {
+      reply.code(400).send({
+        error: "Done is required in the body",
+      });
+    }
+
+    if (typeof body.done !== "number") {
+      reply.code(400).send({
+        error: "Done value in the body must be a boolean",
+      });
+    }
+
+    const habitId = Number(request.params.habitId);
+    if (!habitId || Number.isNaN(habitId)) {
+      reply.code(400).send({
+        error: "habitId params must be a number",
+      });
+    }
+
+    try {
+      const updatedHabit = await updateHabit(habitId, body.done);
+      return updatedHabit;
+    } catch (error) {
+      reply.code(400).send({
+        error: error.message,
+      });
+    }
   });
 }
